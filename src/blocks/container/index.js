4c11317a18e08530654bd153071fcdd4665a76d0
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-08-06 12:05:20 +0800

    Lint fixes

--- src/blocks/container/index.js
+++ src/blocks/container/index.js
@@ -5,50 +5,50 @@ const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const { InnerBlocks } = wp.blockEditor;
 
-registerBlockType( 'custom-block/container' , {
-    title: __( 'Custom Container' ),
+registerBlockType( 'custom-block/container', {
+	title: __( 'Custom Container' ),
 
-    description: __( 'Provide custom container.' ),
+	description: __( 'Provide custom container.' ),
 
-    keywords: [
-        __( 'container' ),
-        __( 'wrapper' ),
-        __( 'section' ),
-    ],
+	keywords: [
+		__( 'container' ),
+		__( 'wrapper' ),
+		__( 'section' ),
+	],
 
-    supports: {
-        align: [ 'wide', 'full' ],
-        anchor: true,
-        html: false,
-    },
+	supports: {
+		align: [ 'wide', 'full' ],
+		anchor: true,
+		html: false,
+	},
 
-    category: 'common',
+	category: 'common',
 
-    icon: 'editor-kitchensink',
+	icon: 'editor-kitchensink',
 
-    attributes: {
-        content: { 
-            type: 'string' 
-        },
-    },
+	attributes: {
+		content: {
+			type: 'string',
+		},
+	},
 
-    edit: ( props ) => {
-        const { className } = props;
+	edit: ( props ) => {
+		const { className } = props;
 
-        return (
-            <div className={ className }>
-                <InnerBlocks renderAppender = { InnerBlocks.ButtonBlockAppender } />
-            </div>
-        );
-    },
+		return (
+			<div className={ className }>
+				<InnerBlocks renderAppender={ InnerBlocks.ButtonBlockAppender } />
+			</div>
+		);
+	},
 
-    save: ( props ) => {
-        const { className } = props;
+	save: ( props ) => {
+		const { className } = props;
 
-        return (
-            <div className={ className }>
-                <InnerBlocks.Content />
-            </div>
-        );
-    },
-});
+		return (
+			<div className={ className }>
+				<InnerBlocks.Content />
+			</div>
+		);
+	},
+} );
